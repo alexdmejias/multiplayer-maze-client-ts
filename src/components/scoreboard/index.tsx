c7ecc8ce050e87f8b2e116a9753757f133eff8c0
Alex Mejias <alex.mejias@bamtechmedia.com>
2019-05-08 23:57:27 -0400

    scoreboard changes

--- src/components/scoreboard/index.tsx
+++ src/components/scoreboard/index.tsx
@@ -5,18 +5,20 @@ import {ScoreboardState} from '../../store/scoreboard/types';
 import { PlayerState } from '../../store/player/types';
 
 type ScoreboardProps = {
-  scoreboard: ScoreboardState
-  playerId: string
+  scoreboard: ScoreboardState;
+  playerId: string;
+  playerUsername: string;
+  playerCurrentScore: number;
 }
 
 class Scoreboard extends React.Component<ScoreboardProps> {
   render() {
-    const {scoreboard, playerId} = this.props;
+    const {scoreboard, playerId, playerUsername, playerCurrentScore} = this.props;
 
     return (
       <div className="scoreboard panel">
         <h1>Scoreboard:</h1>
-        <p>{playerId}</p>
+        <p>{playerUsername}</p>
         <ol>
           {scoreboard.ids.map(currId => {
             const {username, currentScore} = scoreboard.byId[currId];
@@ -30,7 +32,9 @@ class Scoreboard extends React.Component<ScoreboardProps> {
 
 const mapStateToProps = (state: AppStoreState) => ({
   scoreboard: state.scoreboard,
-  playerId: state.player.id
+  playerId: state.player.id,
+  playerUsername: state.player.username,
+  playerCurrentScore: state.player.currentScore
 })
 
 export default connect(mapStateToProps, null)(Scoreboard);
