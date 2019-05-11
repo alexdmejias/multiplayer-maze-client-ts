a42662d9850d60b3caac38c244f4b6a7a9dab551
Alex Mejias <alex.mejias@bamtechmedia.com>
2019-05-11 19:15:30 -0400

    scoreboard is now a stateless component

--- src/components/scoreboard/index.tsx
+++ src/components/scoreboard/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {AppStoreState} from '../../store';
 import {ScoreboardState} from '../../store/scoreboard/types';
-import { PlayerState } from '../../store/player/types';
 
 type ScoreboardProps = {
   scoreboard: ScoreboardState;
@@ -11,24 +10,21 @@ type ScoreboardProps = {
   playerCurrentScore: number;
 }
 
-class Scoreboard extends React.Component<ScoreboardProps> {
-  render() {
-    const {scoreboard, playerId, playerUsername, playerCurrentScore} = this.props;
-
-    return (
-      <div className="scoreboard panel">
-        <h1>Scoreboard:</h1>
-        <p>{playerUsername}</p>
-        <ol>
-          {scoreboard.ids.map(currId => {
-            const {username, currentScore} = scoreboard.byId[currId];
-            return <li key={currId} className={currId === playerId ? 'current' : ''}>{username} - {currentScore}</li>
-          })}
-        </ol>
-      </div>
-    );
-  }
-}
+const Scoreboard = ({scoreboard, playerId, playerUsername}: ScoreboardProps) => {
+  return (
+    <div className="scoreboard panel">
+      <h1>You:</h1>
+      <p>{playerUsername}</p>
+      <h1>Top 50:</h1>
+      <ol>
+        {scoreboard.ids.map(currId => {
+          const {username, currentScore} = scoreboard.byId[currId];
+          return <li key={currId} className={currId === playerId ? 'current' : ''}>{username} - {currentScore}</li>
+        })}
+      </ol>
+    </div>
+  );
+};
 
 const mapStateToProps = (state: AppStoreState) => ({
   scoreboard: state.scoreboard,
